f782e0289f8b417a0c27efcdf01b5e5142c37401
g4b7i3l <gabiibagna@gmail.com>
2021-06-23 15:19:01 -0300

    add estructuring

--- Sprint2/app.js
+++ Sprint2/app.js
@@ -1,3 +1,4 @@
+const { captureRejectionSymbol } = require('events');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,6 +7,10 @@ app.use(express.static('Sprint2/public'));
 
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname,'./views','home.html')));
+app.get('/carrito', (req, res)=> res.sendFile(path.join(__dirname,'./views','carrito.html')));
+app.get('/detalleDeProducto', (req, res)=> res.sendFile(path.join(__dirname,'./views','detalleDeProducto.html')));
+app.get('/login', (req, res)=> res.sendFile(path.join(__dirname,'./views','login.html')));
+app.get('/registro', (req, res)=> res.sendFile(path.join(__dirname,'./views','registro.html')));
 
 
 app.listen(port,() => console.log('Servidor corriendo en localhost:'+port));
