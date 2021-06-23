d9d66e7b413a9b740e051383798819108dcf348c
g4b7i3l <gabiibagna@gmail.com>
2021-06-23 15:06:20 -0300

    retoques

--- Sprint2/app.js
+++ Sprint2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3030;
-app.use(express.static('public'));
+app.use(express.static('Sprint2/public'));
 
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname,'./views','home.html')));
