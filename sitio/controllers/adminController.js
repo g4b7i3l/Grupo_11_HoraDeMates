17afeddb246f049a807b093980093c5a9270b63f
federicocespedes <fgcespedes1995@gmail.com>
2021-08-09 20:27:34 -0300

    revision addProducts

--- sitio/controllers/adminController.js
+++ sitio/controllers/adminController.js
@@ -1,20 +1,34 @@
 
-const productos = require ('../data/products_db');
+const products = require ('../data/products_db');
 module.exports = {
     add : (req,res) => {
+        console.log('estoy acá')
         return res.render ('addProducts',{    
-         productos
+         products
         })
     },
     edit : (req,res) => {
+      
         return res.render ('editProducts',{    
 
         })},
 
-    store : (req,res) => {
-        return res.send(req.body,{
-
-        })
+        store: (req, res) => {
+            console.log(req.body)
+            console.log(req.query)
+            let product = {
+                id : (products[products.length-1].id + 1),
+                title: req.body.title,
+                price : req.body.price,
+                category: req.body.category,
+                description: req.body.description,
+                image : 'default-image.png',
+            }
+            products.push(product);
+            fs.writeFileSync(productsFilePath,JSON.stringify(products,null,2),'utf-8')
+            return res.redirect('/')
+        },
+    
     }
         
-    }
+    
--- sitio/routes/admin.js
+++ sitio/routes/admin.js
@@ -1,10 +1,10 @@
 var express = require('express');
-const router = express.Router();
+var router = express.Router();
 const {add,edit,store} = require ('../controllers/adminController');
 
 /* GET home page. */
 router.get('/addProducts',add);
-router.post('/addProducts',store);
+router.get('/addProducts',store);
 router.get('/editProducts',edit);
 
 module.exports = router;
