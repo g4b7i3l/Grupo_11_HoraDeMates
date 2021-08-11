e9320f6326400659c9d222e0928b5e722206e9bb
federicocespedes <fgcespedes1995@gmail.com>
2021-08-11 18:49:06 -0300

    correction-addProducts

--- sitio/controllers/adminController.js
+++ sitio/controllers/adminController.js
@@ -1,18 +1,29 @@
+const fs = require ('fs');
+const path = require ('path');
 
-const products = require ('../data/products_db');
+const productsFilePath = path.join(__dirname, '../data/products.json')
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 module.exports = {
-    add : (req,res) => {
-        console.log('estoy acá')
-        return res.render ('addProducts',{    
-         products
+    add: (req, res) => {
+        return res.render('addProducts', {
+            products
         })
     },
-    edit : (req,res) => {
-      
-        return res.render ('editProducts',{    
+    store: (req, res) => {
+            let product = {
+            id: (products[products.length - 1].id + 1),
+            title: req.body.title,
+            price: +req.body.price,
+            category: req.body.category,
+            description: req.body.description,
+            image: 'default-image.png',
+        }
+        products.push(product);
+        fs.writeFileSync(productsFilePath, JSON.stringify(products,null,2),'utf-8')
+        return res.redirect('/admin/addProducts')
+    },
 
-        })},
-        
-        store: (req, res) => {
-        res.send(req.body)}}
-    
+edit: (req,res) =>{
+    return res.render('editProducts')
+}}
