d7cde8b8f974b82cad6af795b92399fbd6bb35f8
federicocespedes <fgcespedes1995@gmail.com>
2021-08-11 21:30:59 -0300

    add multer

--- sitio/routes/admin.js
+++ sitio/routes/admin.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
 const {add,edit,store,update,destroy} = require ('../controllers/adminController');
+const multer = require('multer');
+const path = require('path');
+
+
+const storage = multer.diskStorage({
+    destination : (req,file,callback) => {
+        callback(null,'public/images')
+    },
+    filename : (req,file,callback) => {
+        callback(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage,
+})
+
+
+
+
 
 /* GET home page. */
 router.get('/addProducts',add);
-router.post('/addProducts',store);
+router.post('/addProducts', upload.single('image'),store);
 
 router.get('/editProducts/:id',edit);
 router.put('/editProducts/:id',update);
