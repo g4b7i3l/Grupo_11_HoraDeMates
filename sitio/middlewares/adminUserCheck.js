d79827c20175c65dfc0e4883a211161b17ae66ec
federicocespedes <fgcespedes1995@gmail.com>
2021-09-30 13:01:01 -0300

    arreglo middleware admin

--- sitio/middlewares/adminUserCheck.js
+++ sitio/middlewares/adminUserCheck.js
@@ -1,5 +1,5 @@
 module.exports = (req,res,next) => {
-    if(req.session.userLogin && req.session.userLogin.rol === "admin"){
+    if(req.session.userLogin && req.session.userLogin.rols_id === 2){
         next()
     }else{
         res.redirect('/')
