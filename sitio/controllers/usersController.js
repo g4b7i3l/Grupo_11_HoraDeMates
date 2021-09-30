08cfe8fef6ebe6a4d8cd72e7aab8d16616e6013a
federicocespedes <fgcespedes1995@gmail.com>
2021-09-30 12:16:26 -0300

    cambio de user por admin

--- sitio/controllers/usersController.js
+++ sitio/controllers/usersController.js
@@ -31,7 +31,7 @@ module.exports = {
               email : email,
               birth_date : fecha,
               nameId: nombreId,
-              rols_id: 1
+              rols_id: 2
 
           }).then(result => res.redirect('/')
              
