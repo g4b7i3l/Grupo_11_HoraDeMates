f4f685c9126071fc9745b7859e197aa700aef957
g4b7i3l <gabiibagna@gmail.com>
2021-10-05 16:00:21 -0300

    agregando cambios

--- sitio/database/config/config.js
+++ sitio/database/config/config.js
@@ -1,21 +1,21 @@
 module.exports ={
   development: {
     username: "root",
-    password: "f3d3elriddim",
+    password: "1234",
     database: "horademates_db",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   test: {
     username: "root",
-    password: "f3d3elriddim",
+    password: "1234",
     database: "horademates_db",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   production: {
     username: "root",
-    password: "f3d3elriddim",
+    password: "1234",
     database: "horademates_db",
     host: "127.0.0.1",
     dialect: "mysql"
